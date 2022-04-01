51e34fa1abec435684cf87c57a03441e368e5cd9
guilherme <guilgchaves@protonmail.com>
2022-04-01 16:04:39 -0300

    fix: responsive table

--- src/components/GameList.js
+++ src/components/GameList.js
@@ -7,10 +7,16 @@ const GameList = props => {
     <>
       <tr>
         <td title={name}>{name.length > 18 ? name.slice(0, 18) + `...` : name}</td>
-        <td title={url_game}>{url_game.length > 12 ? url_game.slice(0, 12) + `...` : url_game}</td>
-        <td title={url_img}>{url_img.length > 12 ? url_img.slice(0, 10) + `...` : url_img}</td>
-        <td title={description}>{description.length > 18 ? description.slice(0, 19) + `...` : description}</td>
-        <td>{category}</td>
+        <td className="d-none d-sm-table-cell" title={url_game}>
+          {url_game.length > 12 ? url_game.slice(0, 12) + `...` : url_game}
+        </td>
+        <td className="d-none d-sm-table-cell" title={url_img}>
+          {url_img.length > 12 ? url_img.slice(0, 10) + `...` : url_img}
+        </td>
+        <td className="d-none d-sm-table-cell" title={description}>
+          {description.length > 18 ? description.slice(0, 19) + `...` : description}
+        </td>
+        <td className="d-none d-sm-table-cell">{category}</td>
         <td>
           <Link to={{ pathname: '/edit', state: { game: props.game } }}>
             <a href="#" className="btn btn-dark">
